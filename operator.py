26aebd6845f8a3beacb4d13d5099f9f0d5c2f898
anadi5233 <99736035+anadi5233@users.noreply.github.com>
2023-04-05 19:28:05 +0530

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,58 @@
+a = 3
+b = 4
+
+# arithmatic operator
+print("the value of 3+4 is ", 3+4)
+print("the value of 3-4 is ", 3-4)
+print("the value of 3*4 is ", 3*4)
+print("the value of 3/4 is ", 3/4)
+
+# assignment operator 
+a = 34
+a += 12
+print(a)
+
+a = 34
+a -= 12
+print(a)
+
+a = 34
+a *= 12
+print(a)
+
+a = 34
+a/= 12
+print(a)
+
+a = 34
+a /= 12
+print(a)
+
+a = 34
+a -= 12
+a += 12
+print(a)
+
+# comparision operator 
+b = (4>7)
+print(b)
+
+b = (14>=7)
+print(b)
+
+b =(14<=7)
+print(b)
+
+b = (14==7)
+print(b)
+
+b = (14!= 7)
+print(b)
+
+# logical  operators
+
+bool1 = True
+bool2 = False
+print("the value of bool1 and bool2 is",(bool1 and bool2))
+print("the value of bool1 and bool2 is",(bool1 or bool2))
+print("the value of bool2 is ",(not bool2))
